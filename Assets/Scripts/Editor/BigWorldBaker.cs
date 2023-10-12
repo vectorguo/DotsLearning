6d22e612595c5e6d4b07001cb60ceb3566f6cc45
guozhi <guozhi@xd.com>
2023-10-12 16:18:19 +0800

    更新测试场景

--- Assets/Scripts/Editor/BigWorldBaker.cs
+++ Assets/Scripts/Editor/BigWorldBaker.cs
@@ -7,73 +7,73 @@ using UnityEngine.SceneManagement;
 
 public static class BigWorldBaker
 {
-    [MenuItem("BigWorld/BakeSceneForECS")]
-    private static void BakeForEntity()
-    {
-        var sceneRoot = GameObject.Find("Scene");
-        if (sceneRoot == null)
-        {
-            Debug.LogError("Bake Failed");
-            return;
-        }
+    //[MenuItem("BigWorld/BakeSceneForECS")]
+    //private static void BakeForEntity()
+    //{
+    //    var sceneRoot = GameObject.Find("Scene");
+    //    if (sceneRoot == null)
+    //    {
+    //        Debug.LogError("Bake Failed");
+    //        return;
+    //    }
 
-        var objectGroups = new List<BigWorldObjectGroup>();
+    //    var objectGroups = new List<BigWorldObjectGroup>();
         
-        var meshRenderers = sceneRoot.GetComponentsInChildren<MeshRenderer>();
-        foreach (var meshRenderer in meshRenderers)
-        {
-            if (meshRenderer.sharedMaterial != null && meshRenderer.gameObject.TryGetComponent<MeshFilter>(out var meshFilter) && meshFilter.sharedMesh != null)
-            {
-                var material = meshRenderer.sharedMaterial;
-                var mesh = meshFilter.sharedMesh;
+    //    var meshRenderers = sceneRoot.GetComponentsInChildren<MeshRenderer>();
+    //    foreach (var meshRenderer in meshRenderers)
+    //    {
+    //        if (meshRenderer.sharedMaterial != null && meshRenderer.gameObject.TryGetComponent<MeshFilter>(out var meshFilter) && meshFilter.sharedMesh != null)
+    //        {
+    //            var material = meshRenderer.sharedMaterial;
+    //            var mesh = meshFilter.sharedMesh;
                 
-                var transform = meshRenderer.transform;
-                var position = transform.position;
-                var rotation = transform.rotation;
-                var scale = transform.lossyScale;
+    //            var transform = meshRenderer.transform;
+    //            var position = transform.position;
+    //            var rotation = transform.rotation;
+    //            var scale = transform.lossyScale;
                 
-                var group = objectGroups.Find((g) => g.material == material && g.mesh == mesh);
-                if (group == null)
-                {
-                    group = ScriptableObject.CreateInstance<BigWorldObjectGroup>();
-                    group.material = material;
-                    group.mesh = mesh;
-                    group.count = 1;
-                    group.positions = new List<float3> { new float3(position.x, position.y, position.z) };
-                    group.rotations = new List<quaternion> { new quaternion(rotation.x, rotation.y, rotation.z, rotation.w) };
-                    group.scales = new List<float3> { new float3(scale.x, scale.y, scale.z) };
-                    objectGroups.Add(group);
-                }
-                else
-                {
-                    ++group.count;
-                    group.positions.Add(new float3(position.x, position.y, position.z));
-                    group.rotations.Add(new quaternion(rotation.x, rotation.y, rotation.z, rotation.w));
-                    group.scales.Add(new float3(scale.x, scale.y, scale.z));
-                }
-            }
-        }
+    //            var group = objectGroups.Find((g) => g.material == material && g.mesh == mesh);
+    //            if (group == null)
+    //            {
+    //                group = ScriptableObject.CreateInstance<BigWorldObjectGroup>();
+    //                group.material = material;
+    //                group.mesh = mesh;
+    //                group.count = 1;
+    //                group.positions = new List<float3> { new float3(position.x, position.y, position.z) };
+    //                group.rotations = new List<quaternion> { new quaternion(rotation.x, rotation.y, rotation.z, rotation.w) };
+    //                group.scales = new List<float3> { new float3(scale.x, scale.y, scale.z) };
+    //                objectGroups.Add(group);
+    //            }
+    //            else
+    //            {
+    //                ++group.count;
+    //                group.positions.Add(new float3(position.x, position.y, position.z));
+    //                group.rotations.Add(new quaternion(rotation.x, rotation.y, rotation.z, rotation.w));
+    //                group.scales.Add(new float3(scale.x, scale.y, scale.z));
+    //            }
+    //        }
+    //    }
 
-        var scene = SceneManager.GetActiveScene();
-        var scenePath = scene.path.Replace($"{scene.name}.unity", string.Empty) + "/ObjectGroups";
-        if (Directory.Exists(scenePath))
-        {
-            Directory.Delete(scenePath, true);
-        }
+    //    var scene = SceneManager.GetActiveScene();
+    //    var scenePath = scene.path.Replace($"{scene.name}.unity", string.Empty) + "/ObjectGroups";
+    //    if (Directory.Exists(scenePath))
+    //    {
+    //        Directory.Delete(scenePath, true);
+    //    }
 
-        Directory.CreateDirectory(scenePath);
-        for (var i = 0; i < objectGroups.Count; ++i)
-        {
-            AssetDatabase.CreateAsset(objectGroups[i], $"{scenePath}/{i}.asset");
-        }
-        AssetDatabase.Refresh();
-        Debug.LogError("Bake Success");
-    }
+    //    Directory.CreateDirectory(scenePath);
+    //    for (var i = 0; i < objectGroups.Count; ++i)
+    //    {
+    //        AssetDatabase.CreateAsset(objectGroups[i], $"{scenePath}/{i}.asset");
+    //    }
+    //    AssetDatabase.Refresh();
+    //    Debug.LogError("Bake Success");
+    //}
 
     [MenuItem("BigWorld/BakeGrassForBRG")]
     private static void BakeGrassForBRG()
     {
-        var sceneRoot = GameObject.Find("Scene/Grasses");
+        var sceneRoot = GameObject.Find("Scene");
         if (sceneRoot == null)
         {
             Debug.LogError("Bake Failed");
@@ -123,7 +123,7 @@ public static class BigWorldBaker
         Directory.CreateDirectory(scenePath);
         for (var i = 0; i < objectGroups.Count; ++i)
         {
-            AssetDatabase.CreateAsset(objectGroups[i], $"{scenePath}/grass_{i}.asset");
+            AssetDatabase.CreateAsset(objectGroups[i], $"{scenePath}/{objectGroups[i].mesh.name}_{i}.asset");
         }
         AssetDatabase.Refresh();
         Debug.LogError("Bake Success");
--- Assets/Scripts/Runtime/BigWorldRenderGroup.cs
+++ Assets/Scripts/Runtime/BigWorldRenderGroup.cs
@@ -1,9 +1,11 @@
 using System;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities.UniversalDelegates;
 using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.XR;
 
 public class BigWorldRenderGroup : MonoBehaviour
 {
@@ -24,121 +26,173 @@ public class BigWorldRenderGroup : MonoBehaviour
         }
     }
     #endregion
-    
+
+    #region BatchInfo
+    public class BatchInfo
+    {
+        public GraphicsBuffer instanceData;
+        public int instanceCount;
+        public BatchID batchID;
+        public BatchMeshID meshID;
+        public BatchMaterialID materialID;
+
+        public BatchInfo(BatchRendererGroup brg, BigWorldObjectGroupForBRG group)
+        {
+            var totalSize = c_sizeOfPerInstance * group.count + c_sizeOfBufferHead;
+            instanceData = new GraphicsBuffer(GraphicsBuffer.Target.Raw, totalSize / sizeof(int), sizeof(int));
+            instanceCount = group.count;
+            meshID = brg.RegisterMesh(group.mesh);
+            materialID = brg.RegisterMaterial(group.material);
+
+            //组织数据
+            var localToWorld = new PackedMatrix[group.count];
+            for (var i = 0; i < group.count; ++i)
+            {
+                localToWorld[i] = new PackedMatrix(Matrix4x4.TRS(group.positions[i], group.rotations[i], group.scales[i]));
+            }
+
+            //填充GBuffer
+            uint localToWorldGBufferStartIndex = c_sizeOfPackedMatrix * 2;
+            instanceData.SetData(new[] { Matrix4x4.zero }, 0, 0, 1);
+            instanceData.SetData(localToWorld, 0, (int)(localToWorldGBufferStartIndex / c_sizeOfPackedMatrix), localToWorld.Length);
+
+            //metadata
+            var metadata = new NativeArray<MetadataValue>(1, Allocator.Temp);
+            metadata[0] = new MetadataValue
+            {
+                NameID = Shader.PropertyToID("unity_ObjectToWorld"),
+                Value = 0x80000000 | localToWorldGBufferStartIndex,
+            };
+
+            //add batch
+            batchID = brg.AddBatch(metadata, instanceData.bufferHandle);
+        }
+    }
+    #endregion
+
+    #region 常量数据
+    private const int c_sizeOfMatrix = sizeof(float) * 4 * 4;
+    private const int c_sizeOfPackedMatrix = sizeof(float) * 4 * 3;
+    private const int c_sizeOfFloat4 = sizeof(float) * 4;
+    private const int c_sizeOfPerInstance = (c_sizeOfPackedMatrix + sizeof(int) - 1) / sizeof(int) * sizeof(int);  //确保是sizeof(int)的整数倍
+    private const int c_sizeOfBufferHead = (c_sizeOfMatrix * 2 + sizeof(int) - 1) / sizeof(int) * sizeof(int);     //确保是sizeof(int)的整数倍
+    #endregion
+
     /// <summary>
     /// 草数据
     /// </summary>
-    public BigWorldObjectGroupForBRG grass;
+    public BigWorldObjectGroupForBRG[] objectGroups;
 
     /// <summary>
     /// Player
     /// </summary>
     public Transform player;
+
+    /// <summary>
+    /// 裁剪距离
+    /// </summary>
+    public float cullDistance;
     
     /// <summary>
     /// brg
     /// </summary>
     private BatchRendererGroup m_renderGroup;
 
-    private GraphicsBuffer m_instanceData;
-    private BatchID m_batchID;
-    private BatchMeshID m_meshID;
-    private BatchMaterialID m_materialID;
-
-    private const int c_sizeOfMatrix = sizeof(float) * 4 * 4;
-    private const int c_sizeOfPackedMatrix = sizeof(float) * 4 * 3;
-    private const int c_sizeOfFloat4 = sizeof(float) * 4;
-    private const int c_sizeOfPerInstance = (c_sizeOfPackedMatrix + sizeof(int) - 1) / sizeof(int) * sizeof(int);  //确保是sizeof(int)的整数倍
-    private const int c_sizeOfBufferHead = (c_sizeOfMatrix * 2 + sizeof(int) - 1) / sizeof(int) * sizeof(int);     //确保是sizeof(int)的整数倍
+    /// <summary>
+    /// Batch数据
+    /// </summary>
+    private BatchInfo[] m_batchInfos;
     
     private void Start()
     {
-        if (grass == null || player == null)
+        if (objectGroups == null || objectGroups.Length == 0 || player == null)
         {
             return;
         }
-        
+
+        //创建BRG
         m_renderGroup = new BatchRendererGroup(OnPerformCulling, IntPtr.Zero);
-        m_meshID = m_renderGroup.RegisterMesh(grass.mesh);
-        m_materialID = m_renderGroup.RegisterMaterial(grass.material);
 
-        //分配GBuffer
-        var totalSize = c_sizeOfPerInstance * grass.count + c_sizeOfBufferHead;
-        m_instanceData = new GraphicsBuffer(GraphicsBuffer.Target.Raw, totalSize / sizeof(int), sizeof(int));
-        
-        //
-        var localToWorld = new PackedMatrix[grass.count];
-        for (var i = 0; i < grass.count; ++i)
+        //创建Batch数据
+        m_batchInfos = new BatchInfo[objectGroups.Length];
+        for (var i = 0; i < objectGroups.Length; ++i)
         {
-            localToWorld[i] = new PackedMatrix(Matrix4x4.TRS(grass.positions[i], grass.rotations[i], grass.scales[i]));
+            m_batchInfos[i] = new BatchInfo(m_renderGroup, objectGroups[i]);
         }
-        
-        //填充GBuffer
-        uint localToWorldGBufferStartIndex = c_sizeOfPackedMatrix * 2;
-        m_instanceData.SetData(new[]{Matrix4x4.zero}, 0, 0, 1);
-        m_instanceData.SetData(localToWorld, 0, (int)(localToWorldGBufferStartIndex / c_sizeOfPackedMatrix), localToWorld.Length);
-
-        var metadata = new NativeArray<MetadataValue>(1, Allocator.Temp);
-        metadata[0] = new MetadataValue
-        {
-            NameID = Shader.PropertyToID("unity_ObjectToWorld"),
-            Value = 0x80000000 | localToWorldGBufferStartIndex,
-        };
-        m_batchID = m_renderGroup.AddBatch(metadata, m_instanceData.bufferHandle);
     }
 
     private void OnDisable()
     {
-        m_renderGroup.Dispose();
+        if (m_renderGroup != null)
+        {
+            m_renderGroup.Dispose();
+        }
     }
 
     private unsafe JobHandle OnPerformCulling(BatchRendererGroup rendererGroup, BatchCullingContext cullingContext, BatchCullingOutput cullingOutput, IntPtr userContext)
     {
-        var alignment = UnsafeUtility.AlignOf<long>();
+        var batchCount = m_batchInfos.Length;
+        var instanceCount = 0;
+        foreach (var batchInfo in m_batchInfos)
+        {
+            instanceCount += batchInfo.instanceCount;
+        }
 
+        //DrawCommand
+        var alignment = UnsafeUtility.AlignOf<long>();
         var drawCommands = (BatchCullingOutputDrawCommands*)cullingOutput.drawCommands.GetUnsafePtr();
-        drawCommands->drawCommands = (BatchDrawCommand*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawCommand>(), alignment, Allocator.TempJob);
-        drawCommands->drawCommandCount = 1;
-        drawCommands->drawRanges = (BatchDrawRange*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawRange>(), alignment, Allocator.TempJob);
-        drawCommands->drawRangeCount = 1;
-        drawCommands->visibleInstances = (int*)UnsafeUtility.Malloc(grass.count * sizeof(int), alignment, Allocator.TempJob);
-        drawCommands->visibleInstanceCount = grass.count;
+        drawCommands->drawCommands = (BatchDrawCommand*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawCommand>() * batchCount, alignment, Allocator.TempJob);
+        drawCommands->drawCommandCount = batchCount;
+        drawCommands->visibleInstances = (int*)UnsafeUtility.Malloc(instanceCount * sizeof(int), alignment, Allocator.TempJob);
+        drawCommands->visibleInstanceCount = instanceCount;
         drawCommands->drawCommandPickingInstanceIDs = null;
         drawCommands->instanceSortingPositions = null;
         drawCommands->instanceSortingPositionFloatCount = 0;
-        
-        //填充可见数组
-        var playerPosition = player.position;
-        var visibleCount = 0;
-        for (var i = 0; i < grass.count; ++i)
-        {
-            if (Vector3.SqrMagnitude(playerPosition - grass.positions[i]) <= 64.0f)
-            {
-                drawCommands->visibleInstances[visibleCount] = i;
-                ++visibleCount;
-            }
-        }
-        
-        //初始化draw command
-        var drawCommand = drawCommands->drawCommands;
-        drawCommand->visibleCount = (uint)visibleCount;
-        drawCommand->visibleOffset = 0;
-        drawCommand->batchID = m_batchID;
-        drawCommand->materialID = m_materialID;
-        drawCommand->meshID = m_meshID;
-        drawCommand->submeshIndex = 0;
-        drawCommand->splitVisibilityMask = 0xff;
-        drawCommand->flags = 0;
-        drawCommand->sortingPosition = 0;
 
+        //初始化draw command
+        var playerPosition = player.position;
+        var cullDistanceSq = cullDistance * cullDistance;
+        uint visibleOffset = 0;
+        for (var i = 0; i < m_batchInfos.Length; ++i)
+        {
+            //可见裁剪
+            uint visibleCount = 0;
+            var group = objectGroups[i];
+            for (var j = 0; j < group.count; ++j)
+            {
+                if (Vector3.SqrMagnitude(playerPosition - group.positions[j]) <= cullDistanceSq)
+                {
+                    drawCommands->visibleInstances[visibleOffset + visibleCount] = j;
+                    ++visibleCount;
+                }
+            }
+
+            var batchInfo = m_batchInfos[i];
+            var drawCommand = drawCommands->drawCommands + i;
+            drawCommand->visibleCount = visibleCount;
+            drawCommand->visibleOffset = visibleOffset;
+            drawCommand->batchID = batchInfo.batchID;
+            drawCommand->materialID = batchInfo.materialID;
+            drawCommand->meshID = batchInfo.meshID;
+            drawCommand->submeshIndex = 0;
+            drawCommand->splitVisibilityMask = 0xff;
+            drawCommand->flags = 0;
+            drawCommand->sortingPosition = 0;
+
+            visibleOffset += visibleCount;
+        }
+
+        //DrawRange
+        drawCommands->drawRanges = (BatchDrawRange*)UnsafeUtility.Malloc(UnsafeUtility.SizeOf<BatchDrawRange>(), alignment, Allocator.TempJob);
+        drawCommands->drawRangeCount = 1;
         var drawRange = drawCommands->drawRanges;
         drawRange->drawCommandsBegin = 0;
-        drawRange->drawCommandsCount = 1;
+        drawRange->drawCommandsCount = (uint)batchCount;
         drawRange->filterSettings = new BatchFilterSettings
         {
             renderingLayerMask = 0xffffffff
         };
-        
+
         //return
         return new JobHandle();
     }
